c59ce5b5fcc4109e609b0450a7e86855eb254a64
Henrique Freire <h.freire02@gmail.com>
2020-04-03 20:34:46 -0300

    Change port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import App from './app';
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 App.listen(port, () => {
   console.warn(`App listening on port ${port}`);
