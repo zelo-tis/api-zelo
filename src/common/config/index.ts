2bafeecb5007d97dc946fbc8f4abad990ab1e014
Henrique Freire <h.freire02@gmail.com>
2020-05-05 19:54:36 -0300

    Fixing node env

--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -1,7 +1,12 @@
 // Update with your config settings.
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
-require('dotenv').config();
+const dotenv = require("dotenv");
+
+const envFile = __dirname +  process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
+console.log('envFile', envFile);
+dotenv.config({ path: envFile });
+console.log('process.env', process.env.NODE_ENV, process.env.DB_HOST);
 const config = {
   database: {
     host: process.env.DB_HOST,
